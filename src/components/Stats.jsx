4702772f24b1b0dc97738c07bafb08d9348612a4
Sajed Nemli <sajednemli39@gmail.com>
2025-06-27 00:01:37 +0100

    chore: add Framer motion animation

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,25 +1,37 @@
-import styles from "../styles";
 import { stats } from "../constants";
+
+// Framer Motion imports & animations
+import { motion } from "framer-motion";
 
 const Stats = () => {
   return (
-    <section
-      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-14 my-6`}
-    >
+    <div className="flex flex-col sm:flex-row flex-wrap">
       {stats.map((stat) => (
         <div
-          className={`flex-1 flex flex-row justify-center items-center m-3`}
+          className="flex-1 flex flex-row justify-center items-center m-3"
           key={stat.id}
         >
-          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+          <motion.h4
+            className="font-poppins font-semibold xs:text-6xl text-[30px] xs:leading-[53px] leading-[43px] text-white"
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.7 }}
+            viewport={{ once: true }}
+          >
             {stat.value}
-          </h4>
-          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">
+          </motion.h4>
+          <motion.p
+            className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3"
+            initial={{ opacity: 0, x: -30 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.7, delay: 0.7 }}
+            viewport={{ once: true }}
+          >
             {stat.title}
-          </p>
+          </motion.p>
         </div>
       ))}
-    </section>
+    </div>
   );
 };
 
